6ff3aadecf6efebb33528c14c14a50a2e0c39126
Yogi Adianta <yogiadianta0@gmail.com>
2022-10-03 23:38:35 +0700

    Add Create new goal function

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -19,7 +19,11 @@ const setGoal = asyncHandler( async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({ message: 'Set goals' })
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 // @desc    Update goal
