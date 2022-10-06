cd17693786e111dc295ded6ae98e10f96c98b41d
Yogi Adianta <yogiadianta0@gmail.com>
2022-10-06 14:16:42 +0700

    Register user and hash the password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     name: {
-        type: string,
+        type: String,
         required: [true, 'Please add a name']
     },
     email: {
-        type: string,
+        type: String,
         required: [true, 'Please add a email'],
         unique: true
     },
     password: {
-        type: string,
+        type: String,
         required: [true, 'Please add a password']
     }
 }, {
